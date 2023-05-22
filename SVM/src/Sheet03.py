b430f4eaf71a67daf88a2410875d358d3cc4503f
FreddieHorn <fryderykrog7@gmail.com>
2023-05-22 21:10:39 +0200

    added initial data preprocessing

--- SVM/src/Sheet03.py
+++ SVM/src/Sheet03.py
@@ -6,14 +6,14 @@ import os
 # Global constants
 
 # crop/patch dimensions for the training samples
-width = 64
-height = 128
+WIDTH = 64
+HEIGHT = 128
 
 num_negative_samples = 10 # number of negative samples per image
 train_hog_path = 'train_hog_descs' # the file to which you save the HOG descriptors of every patch
 
-path_train =  '../data/task_data/train/' #
-path_test = '..data/task_data/test/'
+path_train =  './data/task_data/train/' #CHANGE PATHS DEPENDING ON THE WORKING DIR!
+path_test = './data/task_data/test/'
 
 hog_descriptor_size = 3780 # the feature dimension of a hog descriptor for a single block
 
@@ -25,6 +25,71 @@ colorspace = 1 # 1 for color, 0 for gray
 # detections: the bounding boxes of the detections (people)
 # returns None
 
+#slightly changed the img reading functions from sheet2
+def read_txt_file(filename):
+    img_list = []
+    with open(filename, "r") as infile: 
+        for line in infile:
+            line = line.strip()
+            img_list.append(line) #value denotes a positive sample - 1 or a negative one - 0
+    return img_list
+
+def extract_img_data(img_list, DIR, positive): #sample_type is either 1 - positive, 0 - negative
+    X = []
+    Y_labels = []
+    if positive:
+        for i, img_path in enumerate(img_list):
+            try:
+                path = os.path.join(DIR, img_path)
+                img = cv.imread(path, cv.IMREAD_COLOR)
+                center = img.shape
+                x = center[1]/2 - WIDTH/2
+                y = center[0]/2 - HEIGHT/2
+                crop_img = img[int(y):int(y+HEIGHT), int(x):int(x+WIDTH), :]
+                X.append(np.array(crop_img))
+            except Exception as e: # catch "broken" images where width or height is 0, therefore cannot be resized. 
+                print(str(e))
+        Y_labels = np.ones(len(X))
+        return np.array(X), Y_labels
+    else:
+        X = []
+        for i, img_path in enumerate(img_list):
+            try:
+                path = os.path.join(DIR, img_path)
+                img = cv.imread(path, cv.IMREAD_COLOR)
+                patches = extract_patches(img)
+                X.extend(patches)
+            except Exception as e: # catch "broken" images where width or height is 0, therefore cannot be resized. 
+                print(str(e))
+            Y_labels = np.zeros(len(X))
+            return np.array(X), Y_labels
+
+
+def extract_patches(image, num_patches=10, patch_size=(128, 64)):
+    image_height, image_width = image.shape[:2]
+    patch_height, patch_width = patch_size
+
+    patches = []
+
+    for _ in range(num_patches):
+        valid_patch = False
+
+        while not valid_patch:
+            # Generate random patch coordinates
+            top = np.random.randint(0, image_height - patch_height + 1)
+            left = np.random.randint(0, image_width - patch_width + 1)
+            bottom = top + patch_height
+            right = left + patch_width
+
+            # Check if patch exceeds image boundaries
+            if bottom <= image_height and right <= image_width:
+                # Extract the patch from each channel
+                patch = image[top:bottom, left:right, :]
+
+                patches.append(patch)
+                valid_patch = True
+
+    return patches
 
 def task1_1():
 
@@ -35,16 +100,26 @@ def task1_1():
     # In total you should have  (x+10*y) training samples represented as HOG features(x=number of positive images, y=number of negative images),
     # save them and their labels in the path train_hog_path and train_labels in order to load them in section 3 
 
-
-
     print('Task 1.1 - Extract HOG features')
 
 
     # Load image names
-  
+    print(os.getcwd())
     filelist_train_pos = path_train + 'filenamesTrainPos.txt'
     filelist_train_neg = path_train + 'filenamesTrainNeg.txt'
 
+    img_pos_list = read_txt_file(filelist_train_pos)
+    img_neg_list = read_txt_file(filelist_train_neg)
+
+    X_train_pos, Y_train_pos = extract_img_data(img_pos_list, path_test+"/pos/", positive=True)
+    X_train_neg, Y_train_neg = extract_img_data(img_neg_list, path_test+"/neg/", positive=False)
+
+    X_train = np.append(X_train_pos, X_train_neg) #order of elements is maintained 
+    Y_train = np.append(Y_train_pos, Y_train_neg)
+
+    idx = np.random.permutation(len(X_train)) #shuffling the data
+    X_train,Y_train = X_train[idx], Y_train[idx]
+    
 
 def task1_2(): 
     print('Task 1.2 - Train SVM and predict confidence values')
